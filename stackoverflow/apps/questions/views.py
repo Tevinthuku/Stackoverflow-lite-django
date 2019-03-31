9e96b5837829a702bc9dfb7a1573022d1c3a6248
Tevinthuku <tevinthuku@gmail.com>
2019-03-31 22:21:16 +0300

    Understood filters

--- stackoverflow/apps/questions/views.py
+++ stackoverflow/apps/questions/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
@@ -16,6 +18,9 @@ class QuestionsView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = QuestionsSerializer
     pagination_class = QuestionsPagination
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('title', 'description', 'author__username')
+    ordering_fields = ('title', 'author__username')
     renderer_classes = (BaseRenderer,)
     queryset = Question.objects.all()
 
